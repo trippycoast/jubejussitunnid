16c299ae0f49d297fd9da7dcaa4fb19ffabad39a
opilane <opilane@KLASSB306-11.local>
2022-01-26 13:02:27 +0200

    fix links

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,33 +1,30 @@
-const cviSidebar = [
-    {text: "cvi", link:"/cvi/"}, 
-    {text:"brief", link:"/cvi/brief"},
-    {text:"moodboard", link:"/cvi/moodboard"},
 
+   
+  const cviSidebar = [
+    { text: "cvi", link: "/cvi/" },
+    { text: "brief", link: "/cvi/brief" },
+    { text: "moodboard", link: "/cvi/moodboard" },
 ]
-
 const logiSidebar = [
     { text: "logi", link: "/logi/" },
     { text: "Fonditöötlus", link: "/logi/01_fontedit" }
 ]
 
-
-
 module.exports = {
     title: "Blog 1",
     themeConfig: {
         nav: [
-            { text:"Home", link:"/" },
-            { text:"About", link:"/about/" },
+            { text: "Home", link: "/" },
+            { text: "About", link: "/about/" },
+            { text: "Log", link: "/logi/" },
             { text: "CVI", link: "/cvi/" },
-
         ],
-        sidebar: 
+        sidebar:
         {
             "/cvi/": cviSidebar,
             "/cvi/brief": cviSidebar,
             "/logi/": logiSidebar,
             "/logi/01_fontedit": logiSidebar,
         },
-   }
-  };
-
+    }
+};
